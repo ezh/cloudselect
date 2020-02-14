807836b7942b712ed41298a813c51b4fc803a405
Alexey Aksenov <ezh@ezh.msk.ru>
2020-02-14 22:15:40 +0300

    Add release task

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 """List of development tasks."""
 from invoke import task
+from invoke.exceptions import Failure
 
 
 @task
@@ -16,6 +17,39 @@ def doc(cmd):
     cmd.run(
         "find docs/Reference/ -name '*.md' | xargs -n1 sed '$!N; /^\\(.*\\)\\n\\1$/!P; D' '-i' ",
     )
+
+
+@task(help={"version": "Version like vN.N.N"})
+def release(ctx, version):
+    """Create new release."""
+    if not version.startswith("v"):
+        print("Please provide version in format of vN.N.N")
+        return
+
+    print("Release new version {}".format(version))
+    version_num = version[1:]
+    try:
+        ctx.run("git tag")
+        ctx.run(
+            'sed -i \'s#__version__ = ".*"#__version__ = "{}"#\' setup.py'.format(
+                version,
+            ),
+        )
+        ctx.run("git add setup.py")
+        ctx.run("git commit -m 'Release version {}'".format(version_num))
+        ctx.run("git tag -a -m 'Release version {}' {}".format(version_num, version))
+        ctx.run("git push --follow-tags")
+        ctx.run("rm CHANGELOG.md")
+        ctx.run("gren changelog --generate --tags=all && remark CHANGELOG.md -o")
+        ctx.run("git add CHANGELOG.md")
+        ctx.run(
+            "git commit -m 'Update changelog for release version {}'".format(
+                version_num,
+            ),
+        )
+        ctx.run("git push")
+    except Failure as e:
+        print(e)
 
 
 @task
